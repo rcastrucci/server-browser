9f01960e8ce36b43c436b53f5abe65284213ccad
Renne Castrucci <rennecastrucci@gmail.com>
2022-03-31 00:32:41 -0300

    Fixing styles directory in head.php

--- resource/head.php
+++ resource/head.php
@@ -10,7 +10,7 @@
     <meta name="robots" content="noindex">
     <meta name="googlebot" content="noindex">
     <title><?PHP echo($_SESSION['config']->getTitle()); ?></title>
-    <link rel="icon" type="image/png" sizes="32x32" href="../images/favicon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./images/favicon.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/styles.css">
+    <link rel="stylesheet" href="./css/styles.css">
 </head>
