546926e57c3b434416bb6ad64cfb921d90909b09
Renne Castrucci <rennecastrucci@gmail.com>
2022-04-02 19:40:29 -0300

    First design draft

--- js/functions.js
+++ js/functions.js
@@ -8,8 +8,90 @@ function submitForm() {
     form.submit();
 }
 
+function setWindowContentSize(percentage, element, speed) {
+    let vh = window.innerHeight - 190;
+    const container = document.querySelectorAll('div.window-container');
+    
+    /* SET HEIGHT WITH PERCENTAGE */
+    document.querySelectorAll('div.window-content').forEach(div => div.style.height = vh*percentage/102+'px');
 
+    /* SET WIDTH */
+    if (percentage === 100) {
+        container.forEach(div => {
+            div.removeAttribute('class');
+            div.setAttribute('class', 'window-container col-12 m-0 p-0');
+        });
+    } else {
+        container.forEach(div => {
+            div.removeAttribute('class');
+            div.setAttribute('class', 'window-container col-12 col-sm-12 col-md-9 col-lg-7 col-xl-6');
+        });
+    }
+
+    if (speed > 0) {
+        element.style.transition = "all 200ms";
+        setTimeout(() => {
+            let offset = centerElement(element);
+            element.style.left = (offset[0]) + 'px';
+            element.style.top  = (offset[1]) + 'px';
+            setTimeout(() => {
+                element.style.transition = '0s';
+            }, speed);
+        }, speed);
+    }
+}
+
+function centerElement(element) {
+    let elementWidth = element.offsetWidth;
+    let elementHeight = element.offsetHeight;
+    let windowWidth = window.innerWidth;
+    let windowHeight = window.innerHeight;
+    return [(windowWidth - elementWidth)/2, (windowHeight - elementHeight)/2];
+}
+
+const linksHref = document.querySelectorAll('[href]');
+const urlGet = new URL(window.location.href);
+const ms = urlGet.searchParams.get("ms");
+const mt = urlGet.searchParams.get("mt");
+const ws = urlGet.searchParams.get("ws");
+const divWindowHead = document.querySelector('.window-head');
+const divWindow = document.querySelector('.window-container');
+let windowSize = 50;
+let mousePosition;
+let offset;
+let isDown = false;
+const btnClose = document.getElementById('btn_close');
+const btnMin = document.getElementById('btn_min');
+const btnMax = document.getElementById('btn_max');
 const buildingMsg = document.querySelectorAll('[building-msg]');
+const windowBtns = document.querySelectorAll('[data-hover]');
+
+window.addEventListener('resize', function(event) {
+    setWindowContentSize(windowSize, divWindow, 200);
+}, true);
+
+btnClose.onclick = () => {
+    window.location = 'index.php?logout';
+}
+
+btnMin.onclick = () => {
+    setWindowContentSize(windowSize = 50, divWindow, 200);
+}
+
+btnMax.onclick = () => {
+    if (windowSize === 50) windowSize = 90;
+    else if (windowSize === 90) windowSize = 100;
+    else windowSize = 50;
+    setWindowContentSize(windowSize, divWindow, 200);
+}
+
+/* WINDOW BUTTONS HOVER */
+windowBtns.forEach(element => {
+    let start = element.src;
+    let hover = element.getAttribute('data-hover');
+    element.onmouseover = () => { element.src = hover; }
+    element.onmouseout  = () => { element.src = start; }
+});
 
 /* TOAST MESSAGES */
 buildingMsg.forEach(element => element.addEventListener('click', () => {
@@ -24,3 +106,50 @@ buildingMsg.forEach(element => element.addEventListener('click', () => {
     document.querySelectorAll('div.toast-body').forEach(div => div.innerHTML = msg);
     new bootstrap.Toast(document.getElementById('toast')).show();
 }));
+
+divWindowHead.addEventListener('mousedown', function(e) {
+    isDown = true;
+    offset = [
+        divWindow.offsetLeft - e.clientX,
+        divWindow.offsetTop - e.clientY
+    ];
+}, true);
+
+document.addEventListener('mouseup', function() {
+    isDown = false;
+}, true);
+
+document.addEventListener('mousemove', function(event) {
+    event.preventDefault();
+    if (isDown) {
+        mousePosition = {    
+            x : event.clientX,
+            y : event.clientY
+        };
+        divWindow.style.left = (mousePosition.x + offset[0]) + 'px';
+        divWindow.style.top  = (mousePosition.y + offset[1]) + 'px';
+    }
+}, true);
+
+/* PASS CORDENATE THRU URL GET */
+linksHref.forEach(link => link.addEventListener('click', () => {
+    link.href = link.getAttribute('href')+'&ms='+divWindow.style.left+'&mt='+divWindow.style.top+'&ws='+windowSize;
+}));
+
+/* ADJUST WINDOW */
+if (ws != null) windowSize = parseInt(ws);
+setWindowContentSize(windowSize, divWindow, 0);
+
+if ((ms !== null) && (mt !== null)) {
+    divWindow.style.left = ms;
+    divWindow.style.top  = mt;
+} else {
+    offset = centerElement(divWindow);
+    divWindow.style.left = (offset[0]) + 'px';
+    divWindow.style.top  = (offset[1]) + 'px';
+}
+
+window.onload = () => {
+    const sectionWindow = document.querySelector('div.row > section');
+    sectionWindow.classList.remove('d-none');
+}
